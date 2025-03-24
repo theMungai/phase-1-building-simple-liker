e971663b822b5ec8974ed427e87547c3cdb69b67
theMungai <mungaithesimon@gmail.com>
2025-03-24 18:58:56 +0300

    Simple Liker

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+function handleHeartClick(event) {
+  const heart = event.target;
 
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        errorModal.classList.remove('hidden');
+        errorModal.textContent = error;
+
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  } else if (heart.textContent === FULL_HEART) {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+hearts.forEach((heart) => {
+  heart.addEventListener('click', handleHeartClick);
+});
 
 
 
